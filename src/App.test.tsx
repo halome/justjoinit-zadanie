bcf8000113467bdf51135ce688562e65489358b4
Rafal Lowczy <rafallowczy@gmail.com>
2024-04-23 13:56:33 +0200

    uat test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react2UAT_test009/i);
+  const linkElement = screen.getByText(/learn react2UAT_test010/i);
   expect(linkElement).toBeInTheDocument();
 });
